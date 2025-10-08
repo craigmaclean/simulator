0bfb02fafec4efa6cc5e31b7758670c39494859e
craigmaclean <craigmaclean1@gmail.com>
2025-10-08 15:13:31 -0700

    JS12 cards - adjusted height on mobile.

--- components/jumpstart12/JumpStartCard.jsx
+++ components/jumpstart12/JumpStartCard.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 export default function JumpStartCard({ title, description }) {
     return (
-        <div className="flip-card h-[8rem]">
+        <div className="flip-card h-[6rem] md:h-[8rem]">
             <div className="flip-card-inner">
-                <div className="flip-card-front bg-navy text-white flex items-center justify-center px-4">
-                <div className="font-semibold text-card-title uppercase">{title}</div>
+                <div className="flex items-center justify-center px-4 text-white flip-card-front bg-navy">
+                <div className="font-semibold uppercase text-card-title">{title}</div>
                 </div>
-                <div className="flip-card-back bg-gold text-navy flex items-center justify-center px-4">
+                <div className="flex items-center justify-center px-4 flip-card-back bg-gold text-navy">
                 <div className="text-card-desc">{description}</div>
                 </div>
             </div>{/* end .flip-card-inner */}
