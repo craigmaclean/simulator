e2c2ff1c0f0075d4783d608ffb5a1afdb82aaebc
craigmaclean <craigmaclean1@gmail.com>
2025-10-28 12:09:35 -0700

    Added report page and StrategySection component

---
+++ components/report/ReportButton.jsx
@@ -0,0 +1,7 @@
+import { Button } from "../ui/button";
+
+export default function ReportButton() {
+  return (
+    <Button className="inline-flex items-center justify-center font-semibold uppercase rounded-md transition cursor-pointer mb-4 hover:opacity-[0.95] focus:outline-none p-8 text-md">Want Help With This?<span className="block font-normal">Book a Profit Acceleration Session</span></Button>
+  );
+}
